8d708eadaab378cfd32bd2e4972410f19ebae7da
Joao Pedro Oliveira Barbosa <joaopedrov159@hotmail.com>
2025-02-11 15:59:06 -0300

    sum of two

--- missing_number.py
+++ missing_number.py
@@ -1,2 +1,12 @@
 nums = [3,0,1]
 
+def sum_two(self, nums, target):
+    hashMap = {}
+
+    for indx, val in enumerate(nums):
+        diff = target - val
+
+        if diff in hashMap:
+            return [indx, hashMap[diff]]
+
+        hashMap[val] = indx
